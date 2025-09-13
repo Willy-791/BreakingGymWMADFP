49e6a7f1d5ab6dcdac0577cf1d418748a97af915
AldanaMajo <majoaldana178@gmail.com>
2025-09-12 23:28:07 -0600

    Entidad de Usuario.

---
+++ BreakingGymWebEN/UsuarioEN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymWebEN
+{
+    public class UsuarioEN : Persona
+    {
+        public string Cuenta { get; set; } // Nombre de la cuenta
+        public string Contrasenia { get; set; } //  la contraseña
+        public UsuarioEN() : base()
+        {
+        }
+        public UsuarioEN(int Id, int IdRol, string Nombre, string Apellido, string Celular, string Cuenta, string Contrasenia)
+            : base(Id, IdRol, Nombre, Apellido, Celular)
+        {
+            this.Cuenta = Cuenta;
+            this.Contrasenia = Contrasenia;
+        }
+    }
+}
