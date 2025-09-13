6d17bae0fe3e30e7f833302a6e877ae8feeb906b
AldanaMajo <majoaldana178@gmail.com>
2025-09-12 23:24:37 -0600

    Entidad padre(persona) e hija.

---
+++ BreakingGymWebEN/ClienteEN.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymWebEN
+{
+    public class ClienteEN : Persona
+    {
+        public string Documento { get; set; }
+        public int IdTipoDocumento { get; set; }
+        public int IdTarjeta { get; set; }
+
+        public ClienteEN() : base()
+        {
+            this.Documento = Documento;
+            this.IdTipoDocumento = IdTipoDocumento;
+        }
+
+        public ClienteEN(int Id, int IdRol, int IdTarjeta, int IdTipoDocumento, string Documento, string Nombre, string Apellido, string Celular)
+            : base(Id, IdRol, Nombre, Apellido, Celular)
+        {
+            this.Documento = Documento;
+            this.IdTipoDocumento = IdTipoDocumento;
+            this.IdTarjeta = IdTarjeta;
+        }
+
+    }
+}
---
+++ BreakingGymWebEN/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymWebEN
+{
+    public abstract class Persona
+    {
+        public int Id { get; set; }
+        public int IdRol { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Celular { get; set; }
+        public Persona() { } // constructor vacio
+        public Persona(int Id, int IdRol, string Nombre, string Apellido, string Celular)
+        {
+            this.Id = Id;
+            this.IdRol = IdRol;
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Celular = Celular;
+        }
+    }
+}
