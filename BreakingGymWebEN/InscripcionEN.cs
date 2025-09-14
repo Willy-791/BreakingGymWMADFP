2bd6372427ea0dad2f39605671083bf0725551a4
Alexandra <Alexandra@ALE>
2025-09-13 22:45:05 -0600

    MembresiaEN, ServicioEN, InscripcionEN, TipoDocumentoEN

---
+++ BreakingGymWebEN/InscripcionEN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymWebEN
+{
+    public class InscripcionEN
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public int IdMembresia { get; set; }
+        public int IdEstado { get; set; }
+        public DateTime FechaInscripcion { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+    }
+}
---
+++ BreakingGymWebEN/MembresiaEN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymWebEN
+{
+
+    public class MembresiaEN
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int IdServicio { get; set; }
+        public int Precio { get; set; }
+        public string Duracion { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
