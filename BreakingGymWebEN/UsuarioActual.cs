9e199e52a2b433febda9bbb70048eed895193252
AldanaMajo <majoaldana178@gmail.com>
2025-09-13 23:03:51 -0600

    Subí Usuario Actual

---
+++ BreakingGymWebEN/UsuarioActual.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymEN
+{
+    public static class UsuarioActual
+    {
+        public static string Cuenta { get; set; }            // Solo la cuenta
+        public static UsuarioEN UsuarioLogueado { get; set; } // También todo el usuario si lo necesitas
+    }
+}
