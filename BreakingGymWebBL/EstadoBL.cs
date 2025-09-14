e4e5f9defc28842a70cc652931f5f38df7d30982
Alejo-1000 <alejandroore503@gmail.com>
2025-09-13 21:28:01 -0600

    Hice la EN y Bl de estado y rol

---
+++ BreakingGymWebBL/EstadoBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymWebBL
+{
+    internal class EstadoBL
+    {
+        EstadoDAL estadoDAL = new EstadoDAL();
+        public List<EstadoEN> MostrarEstado()
+        {
+            return estadoDAL.MostrarEstado();
+        }
+        public int GuardarEstado(EstadoEN pestadoEN)
+        {
+            return estadoDAL.AgregarEstado(pestadoEN);
+        }
+        public int EliminarEstado(EstadoEN pestadoEN)
+        {
+            return estadoDAL.EliminarEstado(pestadoEN);
+        }
+        public int ModificarEstado(EstadoEN pestadoEN)
+        {
+            return estadoDAL.ModificarEstado(pestadoEN);
+        }
+    }
+}
---
+++ BreakingGymWebBL/RolBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymWebBL
+{
+    internal class RolBL
+    {
+        public List<RolEN> MostrarRol()
+        {
+            return RolDAL.MostrarRol();
+        }
+        public int GuardarRol(RolEN prolEN)
+        {
+            return RolDAL.AgregarRol(prolEN);
+        }
+        public int EliminarRol(RolEN prolEN)
+        {
+            return RolDAL.EliminarRol(prolEN);
+        }
+        public int ModificarRol(RolEN prolEN)
+        {
+            return RolDAL.ModificarRol(prolEN);
+        }
+    }
+}
