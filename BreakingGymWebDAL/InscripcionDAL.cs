35ac1338467d1ef7a5114013ade5ce8c1d565294
AldanaMajo <majoaldana178@gmail.com>
2025-09-13 23:21:27 -0600

    Clases inscripcion DAL y Documento

---
+++ BreakingGymWebDAL/InscripcionDAL.cs
@@ -0,0 +1,118 @@
+﻿using BreakingGymWebEN;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace BreakingGymWebDAL
+{
+    public class InscripcionDAL
+    {
+        public static List<InscripcionEN> MostrarInscripcion()
+        {
+            List<InscripcionEN> _Lista = new List<InscripcionEN>();
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("MostrarInscripcion", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                IDataReader _reader = _comando.ExecuteReader();
+                while (_reader.Read())
+                {
+                    _Lista.Add(new InscripcionEN
+                    {
+                        Id = _reader.GetInt32(0),
+                        IdCliente = _reader.GetInt32(1),
+                        IdMembresia = _reader.GetInt32(2),
+                        IdEstado = _reader.GetInt32(3),
+                        FechaInscripcion = _reader.GetDateTime(4),
+                        FechaVencimiento = _reader.GetDateTime(5)
+                    });
+                }
+                _conn.Close();
+            }
+            return _Lista;
+        }
+        public static List<InscripcionEN> BuscarInscripcion(String idCliente)
+        {
+            List<InscripcionEN> _Lista = new List<InscripcionEN>();
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("BuscarInscripcion", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@IdCliente", idCliente));
+                IDataReader _reader = _comando.ExecuteReader();
+                while (_reader.Read())
+                {
+                    _Lista.Add(new InscripcionEN
+                    {
+                        Id = _reader.GetInt32(0),
+                        IdCliente = _reader.GetInt32(1),
+                        IdMembresia = _reader.GetInt32(2),
+                        IdEstado = _reader.GetInt32(3),
+                        FechaInscripcion = _reader.GetDateTime(4),
+                        FechaVencimiento = _reader.GetDateTime(5)
+                    });
+                }
+                _conn.Close();
+            }
+            return _Lista;
+        }
+        public static int AgregarInscripcion(InscripcionEN pinscripcionEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("GuardarInscripcion", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@IdCliente", pinscripcionEN.IdCliente));
+                _comando.Parameters.Add(new SqlParameter("@IdMembresia", pinscripcionEN.IdMembresia));
+                _comando.Parameters.Add(new SqlParameter("@IdEstado", pinscripcionEN.IdEstado));
+                _comando.Parameters.Add(new SqlParameter("@FechaInscripcion", pinscripcionEN.FechaInscripcion));
+                _comando.Parameters.Add(new SqlParameter("@FechaVencimiento", pinscripcionEN.FechaVencimiento));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+        }
+
+        public static int EliminarInscripcion(InscripcionEN pinscripcionEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("EliminarInscripcion", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Id", pinscripcionEN.Id));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+        }
+
+        public static int ModificarInscripcion(InscripcionEN pinscripcionEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("ModificarInscripcion", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Id", pinscripcionEN.Id));
+                _comando.Parameters.Add(new SqlParameter("@IdCliente", pinscripcionEN.IdCliente));
+                _comando.Parameters.Add(new SqlParameter("@IdMembresia", pinscripcionEN.IdMembresia));
+                _comando.Parameters.Add(new SqlParameter("@IdEstado", pinscripcionEN.IdEstado));
+                _comando.Parameters.Add(new SqlParameter("@FechaInscripcion", pinscripcionEN.FechaInscripcion));
+                _comando.Parameters.Add(new SqlParameter("@FechaVencimiento", pinscripcionEN.FechaVencimiento));
+
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+        }
+    }
+}
---
+++ BreakingGymWebDAL/TipoDocumentoDAL.cs
@@ -0,0 +1,81 @@
+﻿using BreakingGymEN;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace BreakingGymDAL
+{
+    public class TipoDocumentoDAL
+    {
+        public static List<TipoDocumentoEN> MostrarTipoDocumento()
+        {
+            List<TipoDocumentoEN> _Lista = new List<TipoDocumentoEN>();
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("MostrarTipoDocumento", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                IDataReader _reader = _comando.ExecuteReader();
+                while (_reader.Read())
+                {
+                    _Lista.Add(new TipoDocumentoEN
+                    {
+                        Id = _reader.GetInt32(0),
+                        Nombre = _reader.GetString(1)
+
+                    });
+                }
+                _conn.Close();
+            }
+            return _Lista;
+        }
+
+        public static int AgregarTipoDocumento(TipoDocumentoEN pTipoDocumentoEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("GuardarTipoDocumento", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Nombre", pTipoDocumentoEN.Nombre));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+        }
+
+        public static int EliminarTipoDocumento(TipoDocumentoEN pTipoDocumentoEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("EliminarTipoDocumento", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Id", pTipoDocumentoEN.Id));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+        }
+
+        public static int ModificarTipoDocumento(TipoDocumentoEN pTipoDocumentoEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("ModificarTipoDocumento", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Id", pTipoDocumentoEN.Id));
+                _comando.Parameters.Add(new SqlParameter("@Nombre", pTipoDocumentoEN.Nombre));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+        }
+    }
+}
