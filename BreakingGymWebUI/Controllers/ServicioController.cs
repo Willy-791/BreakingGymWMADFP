be9b1a27f6b2fa01a08760aa505f2dfc2b9dfd0f
Alexandra <Alexandra@ALE>
2025-09-17 14:16:25 -0600

    Se subio Servicio y la vista

---
+++ BreakingGymWebUI/Controllers/ServicioController.cs
@@ -0,0 +1,21 @@
+﻿using BreakingGymWebEN;
+using BreakinGymWebBL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BreakingGymWebUI.Controllers
+{
+    public class ServicioController : Controller
+    {
+        public IActionResult Index()
+        {
+            var servicioBL = new ServicioBL();
+            var lista = servicioBL.MostrarServicio();
+
+            if (lista == null)
+                lista = new List<ServicioEN>();
+
+
+            return View("Index", lista);
+        }
+    }
+}
