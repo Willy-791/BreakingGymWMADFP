a1d19894dc4ac4a2415f287f11581d0d48686eb8
David25-gif <mp25002@esfe.agape.edu.sv>
2025-09-16 22:37:43 -0600

    se hizo el controlador de membresia y su respectiva vista

---
+++ BreakingGymWebUI/Controllers/MembresiaController.cs
@@ -0,0 +1,26 @@
+﻿using BreakinGymWebBL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BreakingGymWeb.Controllers
+{
+    public class MembresiaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var lista = new MembresiaBL().MostrarMembresia();
+            return View(lista);
+        }
+
+        public IActionResult Ver(int id)
+        {
+            var membresia = MembresiaBL.ObtenerMembresiaPorId(id);
+
+            if (membresia == null)
+            {
+                return NotFound();
+            }
+
+            return View(membresia);
+        }
+    }
+}
